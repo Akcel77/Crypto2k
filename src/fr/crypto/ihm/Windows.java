5bec2b84150644b1e8792193ab50a3d675c95728
Akcel77 <Akeucel@gmail.com>
2021-05-20 12:23:14 +0200

    Interface graphique V1

--- src/fr/crypto/ihm/Windows.java
+++ src/fr/crypto/ihm/Windows.java
@@ -8,25 +8,126 @@ import java.awt.*;
  */
 public class Windows extends JFrame {
 
-    private JPanel mainPanel;
+    private JTextField message, clef ;
+    private JRadioButton radioCrypter, radioDecrypter;
+    private ButtonsPanel btns;
+
+    private JPanel mainPanel, radioType;
 
     public Windows(String title){
         super(title);
-        this.setContentPane(getMainPanel());
+
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(new Dimension(500,500));
+        this.setSize(new Dimension(800,500));
         this.setLocationRelativeTo(null);
-
-        //TODO add components
-
         this.setVisible(true);
+        contentInit();
     }
 
     // Getter
-    public JPanel getMainPanel() {
-        if(mainPanel == null){
-            this.mainPanel = new JPanel();
+
+
+    public void contentInit(){
+
+        //Initialisation du Layout
+        this.mainPanel = new JPanel();
+        mainPanel.setLayout( new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //ICI margin de depart pour chaques items
+        gbc.insets = new Insets(3 ,3 ,3 ,3);
+
+
+        //  gridx correspond au colonne
+        //  gridy a la ligne
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        mainPanel.add(new JLabel("Message"),gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        mainPanel.add(getMessage(),gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        mainPanel.add(new JLabel("Clef de cryptage"),gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        mainPanel.add(getClef(),gbc);
+
+        //Ici j'instancie de nouvelle marge
+        gbc.insets = new Insets(20 ,3 ,3 ,3);
+        gbc.gridy = 5;
+        gbc.gridx = 1;
+        mainPanel.add(new JLabel(""),gbc);
+        gbc.gridx=2;
+        gbc.gridheight = 1;
+        mainPanel.add(getRadioType(),gbc);
+
+        gbc.insets = new Insets(30 ,3 ,3 ,3);
+        gbc.gridy =6;
+        gbc.gridx =2;
+        gbc.gridwidth = 2;
+        mainPanel.add(getBtns(), gbc);
+
+
+        setContentPane(mainPanel);
+
+
+    }
+
+
+    /**
+     *GETTER DES BUTTONS
+     */
+    public JTextField getMessage() {
+        if (message == null){
+            message = new JTextField(30);
         }
-        return mainPanel;
+        return message;
+    }
+    public JTextField getClef() {
+        if (clef == null){
+            clef = new JTextField(30);
+        }
+        return clef;
+    }
+
+    public JRadioButton getRadioCrypter() {
+        if (radioCrypter == null){
+            radioCrypter = new JRadioButton("Crypter message");
+        }
+        return radioCrypter;
+    }
+
+    public JRadioButton getRadioDecrypter() {
+        if (radioDecrypter == null){
+            radioDecrypter = new JRadioButton("Decrypter message");
+        }
+        return radioDecrypter;
+    }
+    /**
+     * Regroupe les radios type sur un seul bouton
+     * @return les radios btns
+     */
+    public JPanel getRadioType() {
+        if (radioType == null){
+            radioType = new JPanel();
+            radioType.setLayout(new BoxLayout(radioType, BoxLayout.Y_AXIS));
+            radioType.add(getRadioCrypter());
+            radioType.add(getRadioDecrypter());
+            ButtonGroup buttonGroup = new ButtonGroup();
+            buttonGroup.add(getRadioCrypter());
+            buttonGroup.add(getRadioDecrypter());
+        }
+        return radioType;
+    }
+
+
+    public ButtonsPanel getBtns() {
+        if (btns == null){
+            btns = new ButtonsPanel();
+
+        }
+        return btns;
     }
 }
--- src/fr/crypto/test/Luncher.java
+++ src/fr/crypto/test/Luncher.java
@@ -2,8 +2,25 @@ package fr.crypto.test;
 
 import fr.crypto.ihm.Windows;
 
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+
 public class Luncher {
-    public static void main(String[] args) {
-        Windows luncher = new Windows("Crypto2000");
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable(){
+
+
+                //Run l'application
+                @Override
+                public void run(){
+                    try {
+                        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+                    } catch (UnsupportedLookAndFeelException e) {
+                        e.printStackTrace();
+                    }
+                    Windows luncher = new Windows("Crypto2000");
+                }
+            });
+        }
     }
-}
